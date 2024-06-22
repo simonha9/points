e69c65b2a305f9eef0d8409d5e78f177f1e3e9a7
Simon H <simon9ha@gmail.com>
2024-06-22 13:35:24 -0400

    add button styling for the points

---
+++ src/components/PointItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export interface PointItemProps {
+    name: string;
+    text: string;
+    add: boolean;
+    points: number;
+    onDataChange: (data: number) => void;
+}
+
+
+
+const PointItem: React.FC<PointItemProps> = ({ name, text, add, points, onDataChange }) => {
+
+    const handleClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const targetElement = event.target as HTMLButtonElement;
+        const re = /(?<=:.[-+])\d+/gm;
+        const points = targetElement.textContent?.match(re) || 0;
+        const modifier = add ? 1 : -1;
+        console.log("points modifier", points, modifier, targetElement.textContent?.match(re))
+        const delta = modifier * +points;
+        console.log("points modifier delta", points, modifier, delta);
+        await fetch("/api/points", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ delta }),
+        });
+        onDataChange(delta);
+    }
+
+
+    return (
+        <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border-2 border-blue-700 rounded-lg" onClick={handleClick} >
+                <b>{name}</b>: {add ? '+' : '-'}{points}
+            </button>
+        </div>
+    )
+}
+
+export default PointItem
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import PointItem from "@/component/PointItem";
+import PointItem from "@/components/PointItem";
 import { useState, useEffect, MouseEventHandler } from "react";
 
 
@@ -53,23 +53,12 @@ export default function Home(props: any) {
   return (
     <div>
       <h1>Points: {data.points}</h1>
-      <div className="btn">
-        <button
-          onClick={addButtonHandler}
-        >
-          Add Points
-        </button>
-        <button
-          onClick={subtractButtonHandler}
-        >
-          Subtract Points
-        </button>
+      <h3>Points System: </h3>
+      <br />
+      <h4>Add points</h4>
+      <div className="flex bg-gray-100">
+        <div className="flex-1 grid grid-cols-3 gap-4 p-4">
 
-      </div>
-      <div>
-        <h3>Points System: </h3>
-        <h4>Add points</h4>
-        <ul>
           <PointItem name="Coding 10min" text="Coding 10min" add={true} points={1} onDataChange={handleDataChange} />
           <PointItem name="Reading Coding blog" text="Reading Coding blog" add={true} points={2} onDataChange={handleDataChange} />
           <PointItem name="Researching" text="Researching" add={true} points={2} onDataChange={handleDataChange} />
@@ -78,15 +67,16 @@ export default function Home(props: any) {
           <PointItem name="Errands" text="Errands" add={true} points={3} onDataChange={handleDataChange} />
           <PointItem name="Brainstorming" text="Brainstorming" add={true} points={2} onDataChange={handleDataChange} />
           <PointItem name="Stretching" text="Stretching" add={true} points={1} onDataChange={handleDataChange} />
-        </ul>
-        <br />
-
-        <h4>Subtract points</h4>
-        <ul>
+        </div>
+      </div>
+      <br />
+      <h4>Subtract points</h4>
+      <div className="flex bg-gray-100">
+        <div className="flex-1 grid grid-cols-3 gap-4 p-4">
           <PointItem name="Game" text="Game" add={false} points={5} onDataChange={handleDataChange} />
           <PointItem name="Watching Youtube" text="Watching Youtube" add={false} points={2} onDataChange={handleDataChange} />
           <PointItem name="Social Media" text="Social Media" add={false} points={1} onDataChange={handleDataChange} />
-        </ul>
+        </div>
       </div>
     </div>
   );
